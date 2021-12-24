185387ec32f3eda3f367ce8c1b5bc6a1db13d65d
yexiulian@yidianlife.com <yexiulian@yidianlife.com>
2021-12-24 11:57:14 +0800

    删除个人中心

--- src/api/user.js
+++ src/api/user.js
@@ -12,15 +12,3 @@ export const login = data => request({
   method: 'POST',
   data
 })
-// 获取商城信息
-export const getStoreDetail = data => request({
-  url: '/disneyH5Store/getStoreDetailYiDongDuan',
-  method: 'POST',
-  data
-})
-// 工行免登
-export const elifeLogin = data => request({
-  url: '/autoLogin/elife',
-  method: 'POST',
-  data
-})
