2bd7fc25b7f058316f2c2af29959e2024924edea
yexiulian@yidianlife.com <yexiulian@yidianlife.com>
2021-12-29 17:20:03 +0800

    12.29

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="content">
-    <van-nav-bar
+    <!-- <van-nav-bar
       fixed
       left-arrow
       title="迪士尼奇梦卡专区"
       @clickRight="() => router.push('/user/personal')"
-    />
+    /> -->
 
     <van-swipe class="my-swipe" :autoplay="3000" indicator-color="white">
       <van-swipe-item>1</van-swipe-item>
@@ -14,46 +14,140 @@
       <van-swipe-item>4</van-swipe-item>
     </van-swipe>
 
+    <van-grid :column-num="5" :border="false">
+      <van-grid-item 
+        v-for="item in tabList" 
+        :key="item.key" 
+        :icon="item.icon" 
+        :to="item.url" 
+        :text="item.title" 
+      />
+    </van-grid>
+
+    <van-divider
+      :style="{ color: '#1989fa',borderColor: '#1989fa',fontWeight: 'bold',padding:' 0 50px' }"
+    >
+      工作时间：09:00 - 20:00
+    </van-divider>
+    <div class="share">
+      <h3>邀请分成</h3>
+      <!-- 每个元素的两侧间隔相等 -->
+      <van-row type="flex" justify="space-around">
+        <van-col span="11"><img style="width:100%" src="@/assets/home_friend.png"/></van-col>
+        <van-col span="11"><img style="width:100%" src="@/assets/home_shangjia.png"/></van-col>
+      </van-row>
+    </div>
+    <!-- 底部 -->
+    <van-tabbar v-model="state.active">
+      <van-tabbar-item icon="home-o" name="home-o" color="#00255">首页</van-tabbar-item>
+      <van-tabbar-item icon="gold-coin-o" name="gold-coin-o">接单</van-tabbar-item>
+      <van-tabbar-item icon="user-o" name="user-o" to="/user/personal">个人</van-tabbar-item>
+    </van-tabbar>
   </div>
 </template>
 
-<script>
-import { defineComponent, onMounted, reactive, toRefs } from 'vue'
+<script setup>
+import { onMounted, reactive } from 'vue'
 import { useRouter } from 'vue-router'
-export default defineComponent({
-  setup() {
-    const router = useRouter()
-    const state = reactive({
-      activeTab: 'Hotel',
-      tabList: [],
-      loading: true,
-      priceShowMethod: 0
-    })
-
-    onMounted(async () => {
-    })
-
-    return {
-      ...toRefs(state),
-      router
-    }
-  }
+const router = useRouter()
+const state = reactive({
+  loading: true,
+  active: 'home-o'
 })
+const tabList = [
+  {
+    key: '1',
+    title: '待操作',
+    icon: `photo-o`,
+    url: '/'
+  },
+  {
+    key: '2',
+    title: '待返款',
+    icon: `photo-o`,
+    url: '/'
+  },
+  {
+    key: '3',
+    title: '待评价',
+    icon: `photo-o`,
+    url: '/'
+  },
+  {
+    key: '4',
+    title: '资金明细',
+    icon: `photo-o`,
+    url: '/'
+  },
+  {
+    key: '5',
+    title: '提现',
+    icon: `photo-o`,
+    url: '/'
+  },
+  {
+    key: '6',
+    title: '待追评',
+    icon: `photo-o`,
+    url: '/'
+  },
+  {
+    key: '7',
+    title: '收款账户',
+    icon: `photo-o`,
+    url: '/'
+  },
+  {
+    key: '8',
+    title: '预售任务',
+    icon: `photo-o`,
+    url: '/'
+  },
+  {
+    key: '9',
+    title: '设置',
+    icon: `photo-o`,
+    url: '/'
+  },
+  {
+    key: '10',
+    title: '收益',
+    icon: `photo-o`,
+    url: '/'
+  }
+]
+
+onMounted(async () => {
+})
+
 </script>
 
 <style lang="less" scoped>
 .content {
   height: 100vh;
-  background: url('@/assets/user/home_bg.png');
-  background-size: 100% 100%;
+  // background: url('@/assets/user/home_bg.png');
+  // background-size: 100% 100%;
   .my-swipe{
-    margin-top: 90px;
     .van-swipe-item {
       color: #fff;
       font-size: 20px;
-      line-height: 150px;
+      line-height: 350px;
       text-align: center;
       background-color: #39a9ed;
+    }
+  }
+  .share{
+    margin: 0 20px;
+    text-align: center;
+    background-color: #20263d;
+    border-radius: 5px;
+    padding: 15px 0;
+    h3{
+      color: rgb(255, 255, 255);
+      font-size: 30px;
+      text-align: center;
+      font-weight: 900;
+      margin: 8px 0 30px;
     }
   }
 }
--- src/views/user/personal.vue
+++ src/views/user/personal.vue
@@ -5,6 +5,54 @@
     @clickLeft="() => router.push('/home')"
   />
   <div class="middle">
+    <div class="card">
+      <div class="top">
+        <img src="@/assets/user_friend.png"/>
+        <div class="top-txt">
+          <h4>名字</h4>
+          <p>ID：555</p>
+          <div>邀请码：无权限</div>
+        </div>
+      </div>
+      <div class="bottom">
+        <div>
+          <h2>10.7</h2>
+          <p>可提现 ></p>
+        </div>
+        <div>
+          <h2>190</h2>
+          <p>已提现（元）</p>
+        </div>
+        <div>
+          <h2>10.7</h2>
+          <p>累计总佣金（元）</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="card tool">
+      <h1>主要工具</h1>
+      <van-grid :column-num="5" :border="false">
+        <van-grid-item icon="orders-o" text="订单" icon-color="#F12D48"/>
+        <van-grid-item icon="friends-o" text="我的好友" icon-color="#FFD252"/>
+        <van-grid-item icon="share-o" text="分享朋友" icon-color="#fc3251"/>
+        <van-grid-item icon="balance-list-o" text="收益" icon-color="#FFCD49"/>
+        <van-grid-item icon="warning-o" text="申诉" icon-color="#989898"/>
+      </van-grid>
+    </div>
+    <div class="card">
+      <img src="@/assets/user_ad.png" style="width:100%"/>
+    </div>
+    <div class="card tool">
+      <h1>常用工具</h1>
+      <van-grid :column-num="4" :border="false">
+        <van-grid-item icon="new" text="新手教程" icon-color="#745AFF"/>
+        <van-grid-item icon="gold-coin" text="收款绑定" icon-color="#FFCD49"/>
+        <van-grid-item icon="manager" text="绑定账号" icon-color="#F0BD84"/>
+        <van-grid-item icon="phone-circle" text="帮助与客服" icon-color="#F12D48"/>
+        <van-grid-item icon="setting" text="设置" icon-color="#F77A85"/>
+      </van-grid>
+    </div>
   </div>
 </template>
 <script setup>
@@ -15,5 +63,55 @@ const router = useRouter()
 <style lang="less" scoped>
 .middle {
   overflow: hidden;
+  margin: 20px 0;
+  .card{
+    background: #fff;
+    border-radius: 15px;
+    padding: 20px;
+    margin:0 30px 20px;
+  }
+  .top{
+    display: flex;
+    align-items: center;
+    font-size: 18px;
+    color: #666;
+    &>img{
+      width: 60px;
+      height: 60px;
+      border-radius: 50%;
+    }
+    &-txt{
+      padding-left: 20px;
+      &>h4{
+        font-weight: 700;
+        font-size: 30px;
+      }
+      &>p{
+        padding: 10px 0;
+      }
+    }
+  }
+  .bottom{
+    display: flex;
+    justify-content: space-around;
+    text-align: center;
+    color: #999;
+    font-size: 14px;
+    margin-top: 50px;
+    h2{
+      font-size: 35px;
+      font-weight: 700;
+      color: #333;
+      margin-bottom: 15px;
+    }
+  }
+  .tool{
+    padding: 0;
+    h1{
+      padding: 30px 0 0 20px;
+      font-weight: 700;
+      font-size: 30px;
+    }
+  }
 }
 </style>
