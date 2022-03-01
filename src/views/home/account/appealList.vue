b3c61a8231d450f2ad5d1d53546a2638fa5255c1
yexiulian@yidianlife.com <yexiulian@yidianlife.com>
2022-03-01 14:25:50 +0800

    2022.3.1

--- src/views/home/account/appealList.vue
+++ src/views/home/account/appealList.vue
@@ -7,73 +7,79 @@
     @clickLeft="() => router.push('/user/personal')"
   />
 
-      <van-list
-        v-model:loading="state.loading"
-        :finished="state.finished"
-        finished-text="没有更多了"
-        @load="loadData"
-      >
-        <div class="item" v-for="item in state.list" :key="item.publishTaskOrderId">
-          <div class="goods">
-            <van-image
-              :src="item.goodsPic"
-              :width="80"
-              :height="80"
+  <van-list
+    v-model:loading="state.loading"
+    :finished="state.finished"
+    finished-text="没有更多了"
+    @load="loadData"
+  >
+    <div class="item" v-for="item in state.list" :key="item.publishTaskOrderId">
+      <div class="goods">
+        <van-image :src="item.goodsPic" :width="80" :height="80" />
+        <div
+          class="goods-name"
+          @click=";(state.show = true), (showDetail = item)"
+        >
+          <div class="price">
+            订单编号：{{ item.publishTaskOrderId || '无' }}
+          </div>
+          <div class="price">申诉类型：{{ item.complaintType || '无' }}</div>
+          <div class="price">申诉日期：{{ item.addTime }}</div>
+          <div class="price">
+            <van-button
+              text="详情"
+              type="primary"
+              size="mini"
+              style="padding: 0 15px"
             />
-            <div class="goods-name">
-              <div class="price">
-                <div>订单编号：{{ item.publishTaskOrderId || '无' }}</div>
-                <!-- <div class="num active">
-                  {{ item.status === 1 ? '已实名' : '未实名' }}
-                </div> -->
-              </div>
-              <div class="price" style="padding: 5px 0">
-                <div>申诉类型：{{ item.complaintType || '无' }}</div>
-              </div>
-              <div class="price" style="padding:0 0 5px 0">
-                <div>状态：{{ item.status === 1 ? '协商中' : item.status === 2 ? '已完结' : '待处理' }}</div>
-              </div>
-              <div class="price" style="padding:0 0 5px 0">
-                <div>发起方：{{ item.startSource === 1 ? '买手' : '商家' }}</div>
-              </div>
-              <div class="price" style="padding:0 0 5px 0">
-                <div>是否平台借入：{{ item.platformStatus === 1 ? '是' : '否' }}</div>
-              </div>
-              <div class="price" style="padding:0 0 5px 0">
-                <div>申诉日期：{{ item.addTime }}</div>
-              </div>
-              <div class="price" style="padding:0 0 5px 0">
-                <div>申诉内容：{{ item.complaintContent }}</div>
-              </div>
-              <!-- <div class="price">
-                <div>完成任务数：0</div>
-                <div
-                  class="num"
-                  @click="(state.show = true), (state.showDetail = item)"
-                >
-                  详情
-                </div>
-              </div> -->
-            </div>
           </div>
         </div>
-      </van-list>
+      </div>
+    </div>
+  </van-list>
   <van-popup v-model:show="state.show" :style="{ width: '77%' }">
+    <div class="header">申诉详情</div>
     <van-cell-group>
-      <van-cell title="ID" :value="state.showDetail?.userId" />
-      <van-cell title="手机" :value="state.showDetail.phone" />
-      <van-cell title="QQ" :value="state.showDetail.qq" />
-      <van-cell title="微信" :value="state.showDetail.weChat" />
-      <!-- <van-cell title="累积做单量" value="0" />
-      <van-cell title="累计收益" value="￥1.52" /> -->
-      <van-cell title="注册时间" :value="state.showDetail.createTime" />
+      <van-cell
+        title="申诉状态:"
+        :value="
+          showDetail.status === 1
+            ? '协商中'
+            : showDetail.status === 2
+            ? '已完结'
+            : '待处理'
+        "
+      />
+      <van-cell
+        title="发起方："
+        :value="showDetail.startSource === 1 ? '买手' : '商家'"
+      />
+      <van-cell
+        title="是否平台借入："
+        :value="showDetail.platformStatus === 1 ? '是' : '否'"
+      />
+      <van-cell title="申诉内容：" :value="showDetail.complaintContent" />
+      <template v-for="item in showDetail.complaintReplyList" :key="item.id">
+        <van-cell>
+          <span class="blue">
+            {{
+              item.replyType === 1
+                ? '商家'
+                : item.replyType === 2
+                ? '买手'
+                : '平台'
+            }}回复：
+          </span>
+          {{ item.replyContent }}
+        </van-cell>
+      </template>
     </van-cell-group>
   </van-popup>
 </template>
 <script setup>
 import moment from 'moment'
 import { useRouter } from 'vue-router'
-import { reactive } from 'vue'
+import { reactive, ref } from 'vue'
 import { complaintListApi } from '@/api/home'
 const router = useRouter()
 const state = reactive({
@@ -82,23 +88,9 @@ const state = reactive({
   list: [],
   page: 0,
   pageSize: 10,
-  show: false,
-  showDetail: {}
+  show: false
 })
-const tabList = [
-  // {
-  //   label: '全部',
-  //   key: 0
-  // },
-  {
-    label: '已实名',
-    key: 1
-  },
-  {
-    label: '未实名',
-    key: 0
-  }
-]
+const showDetail = ref({})
 const loadData = async init => {
   !init
     ? state.page++
@@ -137,12 +129,12 @@ const loadData = async init => {
   padding: 30px;
   .goods {
     display: flex;
-    padding: 30px 0 25px;
+    // padding: 30px 0 25px;
     align-items: center;
     &-name {
       flex: 1;
       padding: 0px 0 0px 30px;
-      font-size: 25px;
+      font-size: 30px;
       display: flex;
       flex-direction: column;
       justify-content: space-between;
@@ -150,6 +142,7 @@ const loadData = async init => {
       .price {
         display: flex;
         align-items: center;
+        padding-bottom: 10px;
         .num {
           font-size: 24px;
           flex: 1;
@@ -162,10 +155,15 @@ const loadData = async init => {
     }
   }
 }
-/deep/ .van-tab--active {
-  color: rgb(242, 145, 0);
+.header {
+  height: 88px;
+  line-height: 88px;
+  font-size: 32px;
+  color: #333;
+  font-weight: bold;
+  text-align: center;
 }
-/deep/ .van-cell__value {
-  color: #ff0036;
+.blue {
+  color: #1989fa;
 }
 </style>
